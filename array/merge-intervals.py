2c0ebce8a0ebaaa0308a1cd6e49965b3d12726d2
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-10-07 21:30:45 -0500

    LC-merge-intervals [Runtime: 8 ms; Memory: 21.8 MB]

--- array/merge-intervals.py
+++ array/merge-intervals.py
@@ -1,12 +1,17 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key= lambda x: x[0])
-        overlap = []
-
-        for interval in intervals:
-            if not overlap or overlap[-1][1] < interval[0]:
-                overlap.append(interval)
+        intervals.sort(key= lambda x:x[0])
+        interval = []
+        curr = []
+        for a, b in intervals:
+            if not curr:
+                curr = [a, b]
             else:
-                overlap[-1] = [overlap[-1][0], max(overlap[-1][1], interval[1])]
-        
-        return overlap
+                if a > curr[-1]:
+                    interval.append(curr)
+                    curr = [a, b]
+                else:
+                    curr[-1] = max(curr[-1], b)
+        if curr:
+            interval.append(curr)
+        return interval
