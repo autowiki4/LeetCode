b7fb0eec2317cb5745a61b5f5dbe35a8a6f5428e
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-10-09 15:08:16 -0500

    LC-reverse-words-in-a-string [Runtime: 0 ms; Memory: 18.1 MB]

--- two-pointers/reverse-words-in-a-string.py
+++ two-pointers/reverse-words-in-a-string.py
@@ -1,8 +1,9 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        s = s.strip()
-        words_list = s.split()
-        words_list = words_list[::-1]
-
-        output = ' '.join(words_list)
-        return output
+        words = s.split()
+        n = len(words)
+        mid = n//2
+        for i in range(mid):
+            words[i], words[n-i-1] = words[n-i-1], words[i]
+        return ' '.join(words)
+        
