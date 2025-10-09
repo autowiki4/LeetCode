41bbb4f12fac927612b8f6e07d98e7c118e63b4d
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-10-09 16:36:01 -0500

    LC-product-of-array-except-self [Runtime: 23 ms; Memory: 26 MB]

--- prefix-sum/product-of-array-except-self.py
+++ prefix-sum/product-of-array-except-self.py
@@ -3,16 +3,13 @@ class Solution:
         l_mult = 1
         r_mult = 1
         n = len(nums)
-
-        right = [0] * n
-        left = [0] * n
-        
-        j = len(nums) - 1
-        for i in range(len(nums)):
-            right[j] = r_mult
-            left[i] = l_mult
+        j = n-1
+        l = [0]*n
+        r = [0]*n
+        for i in range(n):
+            l[i] = l_mult
+            r[j] = r_mult
             l_mult *= nums[i]
             r_mult *= nums[j]
             j -= 1
-        
-        return [l*r for l, r in zip(left, right)]
+        return [left*right for left,right in zip(l, r)]
