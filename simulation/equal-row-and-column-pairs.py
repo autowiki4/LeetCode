c523c8be6d842c19caa78599ae1fa6dc725f86d9
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-10-13 11:05:26 -0500

    LC-equal-row-and-column-pairs [Runtime: 24 ms; Memory: 22 MB]

--- simulation/equal-row-and-column-pairs.py
+++ simulation/equal-row-and-column-pairs.py
@@ -1,15 +1,14 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        row_count = {}
+        equal = {}
         for row in grid:
             row_tuple = tuple(row)
-            row_count[row_tuple] = row_count.get(row_tuple, 0) + 1
-        
-        equal = 0
-        for c in range(n):
-            col_tuple = tuple(grid[i][c] for i in range(n))
-            if col_tuple in row_count:
-                equal += row_count[col_tuple]
-        
-        return equal
+            equal[row_tuple] = equal.get(row_tuple, 0) + 1
+        pairs = 0
+        for i in range(n):
+            col = [grid[j][i] for j in range(n)]
+            col_tuple = tuple(col)
+            if col_tuple in equal:
+                pairs += equal[col_tuple]
+        return pairs
