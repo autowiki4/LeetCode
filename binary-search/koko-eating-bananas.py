02b931444e085ab69a25ee2532ac1191fa817a4d
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-10-14 15:24:04 -0500

    LC-koko-eating-bananas [Runtime: 161 ms; Memory: 19.3 MB]

--- binary-search/koko-eating-bananas.py
+++ binary-search/koko-eating-bananas.py
@@ -1,20 +1,17 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def hour_works(hour):
+        def canEat(hour):
             hours = 0
             for p in piles:
-                hours += math.ceil(p / hour)
-            
+                hours += math.ceil(p/hour)
             return hours <= h
         
         l = 1
         r = max(piles)
-
         while l < r:
-            mid = (l + r) // 2
-
-            if hour_works(mid):
+            mid = (l+r) // 2
+            if canEat(mid):
                 r = mid
             else:
-                l = mid + 1
-        return r
+                l = mid+1
+        return l
