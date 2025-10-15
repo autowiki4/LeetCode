7cfaf99d9a1323252bc96a5f8a92ab3938445f04
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-10-14 21:05:29 -0500

    LC-dota2-senate [Runtime: 7 ms; Memory: 18.2 MB]

--- greedy/dota2-senate.py
+++ greedy/dota2-senate.py
@@ -1,20 +1,20 @@
+from collections import deque
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
         r_que = deque()
         d_que = deque()
         n = len(senate)
 
-        for i, r in enumerate(senate):
-            if r == 'R':
+        for i, v  in enumerate(senate):
+            if v == 'R':
                 r_que.append(i)
             else:
                 d_que.append(i)
         while r_que and d_que:
             r_pos = r_que.popleft()
-            d_pos = d_que.popleft()
-
-            if r_pos<d_pos:
+            l_pos = d_que.popleft()
+            if r_pos < l_pos:
                 r_que.append(r_pos+n)
             else:
-                d_que.append(d_pos+n)
-        return 'Radiant' if r_que else 'Dire'
+                d_que.append(l_pos+n)
+        return "Radiant" if r_que else 'Dire'
