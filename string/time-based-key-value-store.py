c99f29da37613f7739f2f1df41e688b063c3b671
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-10-18 22:40:47 -0500

    LC-time-based-key-value-store [Runtime: 98 ms; Memory: 74.4 MB]

--- string/time-based-key-value-store.py
+++ string/time-based-key-value-store.py
@@ -1,36 +1,27 @@
-from collections import defaultdict
 class TimeMap:
 
     def __init__(self):
-        # Dictionary to store key -> list of (timestamp, value)
-        self.store = defaultdict(list)
+        self.store = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        # Append the (timestamp, value) pair to the list for the key
+        if key not in self.store:
+            self.store[key] = []
         self.store[key].append((timestamp, value))
 
     def get(self, key: str, timestamp: int) -> str:
-        # Get the list of (timestamp, value) for the key
         if key not in self.store:
-            return ""
-        
+            return ''
+        res = ''
         values = self.store[key]
-        # Perform manual binary search for the largest timestamp <= given timestamp
-        left, right = 0, len(values) - 1
-        result = ""  # Default to empty string if no valid timestamp is found
-
-        while left <= right:
-            mid = (left + right) // 2
-            if values[mid][0] <= timestamp:
-                # Found a candidate, move right to find the largest timestamp <= given timestamp
-                result = values[mid][1]
-                left = mid + 1
+        l, r = 0, len(values)-1
+        while l<=r:
+            m = (l+r)//2
+            if values[m][0] <= timestamp:
+                res = values[m][1]
+                l = m+1
             else:
-                # Timestamp is too large, move left
-                right = mid - 1
-
-        return result
-        
+                r = m-1
+        return res
 
 
 # Your TimeMap object will be instantiated and called as such:
