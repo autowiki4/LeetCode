5f4826775c5e11c75d21bab64cdfc5e26d96e074
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-10-25 03:53:29 -0500

    LC-h-index [Runtime: N/A; Memory: N/A]

--- sorting/h-index.py
+++ sorting/h-index.py
@@ -4,7 +4,7 @@ class Solution:
         n = len(citations)
         citations = [0] + citations
         count = [i for i in range(n, 0, -1)]
-        curr = 1
+        curr = 0
         for i in range(1, n+1):
             if citations[i-1] <= count[i-1]:
                 curr = count[i-1]
