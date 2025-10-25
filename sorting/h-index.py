aa33d3d305f3fe14e8a4506e634def6e8abc5e7a
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-10-25 04:04:13 -0500

    LC-h-index [Runtime: 0 ms; Memory: 18.3 MB]

--- sorting/h-index.py
+++ sorting/h-index.py
@@ -1,11 +1,17 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        citations.sort()
         n = len(citations)
-        citations = [0] + citations
-        count = [i for i in range(n, -1, -1)]
-        curr = 0
-        for i in range(n+1):
-            if citations[i] <= count[i]:
-                curr = count[i]
-        return curr
+        buckets = [0] * (n+1)
+        for c in citations:
+            if c >= n:
+                buckets[n] += 1
+            else:
+                buckets[c] += 1
+        count = 0
+        for i in range(n, -1, -1):
+            count += buckets[i]
+            if count >= i:
+                return i
+        return 0
+
+        
