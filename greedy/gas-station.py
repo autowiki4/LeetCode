00c1e59ab57c509e2850c0b86a9d9927989c12cf
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-10-29 16:42:44 -0500

    LC-gas-station [Runtime: 19 ms; Memory: 23.3 MB]

--- greedy/gas-station.py
+++ greedy/gas-station.py
@@ -1,26 +1,15 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        curr = 0
-        index = 0
-        n = len(cost)
-        for i in range(n):
-            if gas[i] > cost[i]:
-                index = i
-                curr = gas[i] - cost[i]
-                break
-            if i == n-1:
-                return -1
-        i = (index + 1) % n
-        curr += gas[i]
-        while True:
-            if i == index:
-                return index
-            if curr < cost[i]:
-                return -1
-            curr -= cost[i]
-            nxt = (i+1) % n
-            curr += gas[nxt]
-            i = nxt
+        if sum(gas) < sum(cost):
+            return -1
+        tank = 0
+        start = 0
+        for i in range(len(gas)):
+            tank += gas[i] - cost[i]
+            if tank < 0:
+                start = i+1
+                tank = 0
+        return start
         
 
 
