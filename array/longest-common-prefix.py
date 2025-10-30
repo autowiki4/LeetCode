e151f5ce9589afe7cb5c92034412e388c4b8248b
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-10-29 23:45:31 -0500

    LC-longest-common-prefix [Runtime: 0 ms; Memory: 17.9 MB]

--- array/longest-common-prefix.py
+++ array/longest-common-prefix.py
@@ -1,22 +1,13 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        if not strs:
-            return ''
-        if len(strs) == 1:
-            return strs[0]
-        
-        min_len = float('inf')
+        longest = min([len(word) for word in strs])
 
-        for s in strs:
-            if len(s) < min_len:
-                min_len = len(s)
-        
-        i = 0
-        while i < min_len:
-            for s in strs:
-                if s[i] != strs[0][i]:
-                    return s[:i]
-            i += 1
-        
-        return strs[0][:i]
-        
+        prefix = ''
+        for i in range(longest):
+            check = set()
+            for word in strs:
+                check.add(word[i])
+            if len(check) > 1:
+                return prefix
+            prefix += check.pop()
+        return prefix
