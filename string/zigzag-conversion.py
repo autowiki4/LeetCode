02c98ca4382efafd5bc56cc71720ebd6f1d150b3
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-10-30 00:32:58 -0500

    LC-zigzag-conversion [Runtime: 4 ms; Memory: 17.9 MB]

--- string/zigzag-conversion.py
+++ string/zigzag-conversion.py
@@ -1,26 +1,13 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if numRows == 1:
+        if numRows == 1 or numRows >= len(s):
             return s
-
-        constant = 2 * numRows - 2
-        output = ''
-
-        def add_constant(start, output_string, rows):
-            index = start
-            while index < len(s):
-                output_string += s[index]
-                # For rows in between the first and last
-                middle = index + constant - 2 * start
-                if start > 0 and start < rows - 1 and middle < len(s):
-                    output_string += s[middle]
-                index += constant
-            return output_string
-
-        for row in range(numRows):
-            output = add_constant(row, output, numRows)
-        
-        return output
-
-
-        
+        rows = [''] * numRows
+        curr = 0
+        down = False
+        for char in s:
+            rows[curr] += char
+            if curr == 0 or curr == numRows - 1:
+                down = not down
+            curr = curr + 1 if down else curr - 1
+        return ''.join(rows)
