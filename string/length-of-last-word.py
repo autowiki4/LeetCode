3493ec9eaf8252a9070e38c144f2ce9223bd22c8
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-10-29 23:37:00 -0500

    LC-length-of-last-word [Runtime: 0 ms; Memory: 17.7 MB]

--- string/length-of-last-word.py
+++ string/length-of-last-word.py
@@ -1,16 +1,10 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        if len(s) == 1 and s[0].isalpha():
-            return 1
-        curr = 0
-        last_len = 0
-        n = len(s)
-        start = n-1
-        while s[start] == ' ' and start >= 0:
-            start -= 1
-        if start == 0:
-            return 0
-        curr = start
-        while s[curr].isalpha():
-            curr -= 1
-        return start - curr
+        i = len(s)-1
+        length = 0
+        while i >= 0 and s[i] == ' ':
+            i -= 1
+        while i >= 0 and s[i] != ' ':
+            i-= 1
+            length += 1
+        return length
