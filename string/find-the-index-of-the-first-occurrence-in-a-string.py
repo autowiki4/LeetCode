6eb1cc8d38525df3a0e1e886a3b56daa8570afa0
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-10-30 00:46:03 -0500

    LC-find-the-index-of-the-first-occurrence-in-a-string [Runtime: 0 ms; Memory: 17.8 MB]

--- string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -2,7 +2,7 @@ class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         length = len(needle)
         n = len(haystack)
-        for i in range(n-length):
+        for i in range(n-length+1):
             if haystack[i:i+length] == needle:
                 return i
         return -1
