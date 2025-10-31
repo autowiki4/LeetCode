affc5cca3e93b690f64a91b905ac23a8348643f1
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-10-31 16:00:37 -0500

    LC-is-subsequence [Runtime: 0 ms; Memory: 17.7 MB]

--- two-pointers/is-subsequence.py
+++ two-pointers/is-subsequence.py
@@ -1,18 +1,10 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        T = len(t)
-        S = len(s)
-        j = 0
+        t_id, s_id = 0, 0
+        s_len, t_len = len(s), len(t)
 
-        if s == '':
-            return True
-        if S > T:
-            return False
-
-        for i in range(T):
-            if s[j] == t[i]:
-                if j == S - 1:
-                    return True
-                j += 1
-        return False
-        
+        while s_id < s_len and t_id < t_len:
+            if s[s_id] == t[t_id]:
+                s_id += 1
+            t_id += 1
+        return s_id == s_len
