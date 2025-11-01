792307ce1e64e74268aa675bcc474eded372941a
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-11-01 01:08:09 -0500

    LC-container-with-most-water [Runtime: 107 ms; Memory: 28.5 MB]

--- greedy/container-with-most-water.py
+++ greedy/container-with-most-water.py
@@ -1,11 +1,11 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         max_water = 0
-        l, r = 0, len(height)-1
+        l, r = 0, len(height) - 1
         while l < r:
-            h= min(height[l], height[r])
-            base = abs(r - l)
-            max_water = max(max_water, h*base)
+            h = min(height[l], height[r])
+            base = abs(r-l)
+            max_water = max(max_water, base * h)
             if height[l] < height[r]:
                 l += 1
             else:
