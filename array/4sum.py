ba78cb4dc991ee7c022f8a33684ce9d98f823154
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-11-01 01:48:15 -0500

    LC-4sum [Runtime: 395 ms; Memory: 17.7 MB]

--- array/4sum.py
+++ array/4sum.py
@@ -16,6 +16,10 @@ class Solution:
                         ans.append([nums[i], nums[j], nums[l], nums[r]])
                         l += 1
                         r -= 1
+                        while l < r and nums[l] == nums[l-1]:
+                            l += 1
+                        while l < r and nums[r] == nums[r+1]:
+                            r -= 1
                     elif add < target:
                         l += 1
                     else:
