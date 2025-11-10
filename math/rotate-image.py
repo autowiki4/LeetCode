0e7acc0ec5c2f6c1f886d7486f783c1d8ab3c858
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-11-10 02:03:50 -0600

    LC-rotate-image [Runtime: 0 ms; Memory: 17.8 MB]

--- math/rotate-image.py
+++ math/rotate-image.py
@@ -3,12 +3,13 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        n = len(matrix)
 
+        n = len(matrix)
         for i in range(n):
-            for j in range(i+1, n):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            for j in range(i):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
         
         for i in range(n):
             for j in range(n // 2):
-                matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
+                matrix[i][j], matrix[i][-j-1] = matrix[i][-j-1], matrix[i][j]
+        
