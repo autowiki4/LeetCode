4eb8d736cc7d32f08357c521a5b659e8afe24df0
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-11-09 22:22:21 -0600

    LC-check-if-every-row-and-column-contains-all-numbers [Runtime: 23 ms; Memory: 18.5 MB]

--- matrix/check-if-every-row-and-column-contains-all-numbers.py
+++ matrix/check-if-every-row-and-column-contains-all-numbers.py
@@ -2,23 +2,10 @@ class Solution:
     def checkValid(self, matrix: List[List[int]]) -> bool:
         n = len(matrix)
         original = set([i for i in range(1, n+1)])
-        columns = [set(original) for _ in range(n)]
-        rows = [set(original) for _ in range(n)]
-
-        for i in range(n):
-            for j in range(n):
-                num = matrix[i][j]
-                if num not in rows[i]:
-                    return False
-                rows[i].remove(num)
-                if num not in columns[j]:
-                    return False
-                columns[j].remove(num)
-        for hashmap in columns:
-            if hashmap:
+        for row in matrix:
+            if set(row) != original:
                 return False
-        for hashmap in rows:
-            if hashmap:
+        for col in range(n):
+            if set([matrix[row][col] for row in range(n)]) != original:
                 return False
         return True
-
