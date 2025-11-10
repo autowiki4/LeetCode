4829621dc34bacd5fa9392d7e45232daa3a60b6c
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-11-09 21:58:39 -0600

    LC-valid-sudoku [Runtime: 7 ms; Memory: 17.8 MB]

--- hash-table/valid-sudoku.py
+++ hash-table/valid-sudoku.py
@@ -1,28 +1,23 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        column_items = [set() for _ in range(9)]
-
-        inner_box_columns = {}
+        columns = [set() for _ in range(9)]
+        inner_box = {}
         for i in range(3):
-            inner_box_columns[i] = [set() for _ in range(3)]
-
+            inner_box[i] = [set() for _ in range(3)]
+        
         for i in range(9):
-            digit_set = set()
+            curr = set()
             for j in range(9):
                 if board[i][j].isnumeric():
-                    if board[i][j] in digit_set:
+                    num = board[i][j]
+                    if num in curr:
                         return False
-                    digit_set.add(board[i][j])
-                    if board[i][j] in column_items[j]:
+                    curr.add(num)
+                    if num in columns[j]:
                         return False
-                    column_items[j].add(board[i][j])
-
-                    if board[i][j] in inner_box_columns[int(i / 3)][int(j / 3)]:
+                    columns[j].add(num)
+                    if num in inner_box[int(i/3)][int(j/3)]:
                         return False
-                    inner_box_columns[int(i / 3)][int(j / 3)].add(board[i][j])
+                    inner_box[int(i/3)][int(j/3)].add(num)
         return True
-
-
-
-        for i in range(0, 9, 3):
-            column_items
+                    
