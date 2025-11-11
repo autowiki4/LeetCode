6713013029c92bb336c5d8d5aad75da361ee8ea8
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-11-11 15:45:14 -0600

    LC-valid-anagram [Runtime: 14 ms; Memory: 18 MB]

--- string/valid-anagram.py
+++ string/valid-anagram.py
@@ -2,13 +2,14 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
+        counter = {}
+        for c in s:
+            counter[c] = counter.get(c, 0) + 1
         
-        s_dict = {}
-        for char in s:
-            s_dict[char] = s_dict.get(char, 0) + 1
-        
-        t_dict = {}
-        for char in t:
-            t_dict[char] = t_dict.get(char, 0) + 1
-        
-        return s_dict == t_dict
+        for c in t:
+            if c not in counter:
+                return False
+            counter[c] -= 1
+            if counter[c] == 0:
+                del counter[c]
+        return len(counter) == 0
