52f090724a906ef2401874101acf284a767c3acd
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-11-11 15:55:03 -0600

    LC-two-sum [Runtime: 3 ms; Memory: 19.5 MB]

--- array/two-sum.py
+++ array/two-sum.py
@@ -1,17 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        self.nums = nums
-        self.target = target
-        sum_dict = {}
-        for i,num in enumerate(self.nums):
-            diff = self.target - num
-            if diff in sum_dict:
-                return [sum_dict[diff], i]
-            else:
-                sum_dict[num] = i 
-
-test_case = Solution()
-print(test_case.twoSum(nums=[2,7,11,15], target = 9))
-
-
-        
+        rem = {}
+        for i in range(len(nums)):
+            num = nums[i]
+            if num in rem:
+                return [rem[num], i]
+            rem[target-num] = i
