4eba57b9d8ff3710210cd84b736c64e08288fdc5
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-11-11 15:50:14 -0600

    LC-group-anagrams [Runtime: N/A; Memory: N/A]

--- array/group-anagrams.py
+++ array/group-anagrams.py
@@ -1,12 +1,9 @@
-from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # ans = defaultdict(list)
         ans = {}
-        for s in strs:
+        for s in str:
             key = tuple(sorted(s))
             if key not in ans:
                 ans[key] = []
             ans[key].append(s)
-        
         return list(ans.values())
