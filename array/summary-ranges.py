77ac2f01c318b36b4d5391ebbfdee92ba8bd703a
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-11-12 02:23:22 -0600

    LC-summary-ranges [Runtime: 0 ms; Memory: 17.7 MB]

--- array/summary-ranges.py
+++ array/summary-ranges.py
@@ -1,26 +1,23 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        ranges = []
-
-        current_range = ''
-
-        for i in range(len(nums)):
-            if i == len(nums) - 1:
-                if current_range:
-                    current_range += str(nums[i])
-                    ranges.append(current_range)
-                else:
-                    ranges.append(str(nums[i]))
-            else:
-                if not current_range:
-                    if nums[i+1] == nums[i] + 1:
-                        current_range = f'{nums[i]}->'
-                    else:
-                        ranges.append(str(nums[i]))
-                else:
-                    if nums[i+1] != nums[i] + 1:
-                        current_range += str(nums[i])
-                        ranges.append(current_range)
-                        current_range = ''
+        if not nums:
+            return []
         
-        return ranges
+        result = []
+        start = nums[0]
+        
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1] + 1:
+                if start == nums[i-1]:
+                    result.append(str(start))
+                else:
+                    result.append(f"{start}->{nums[i-1]}")  
+                start = nums[i]
+        
+        if start == nums[-1]:
+            result.append(str(start))
+        else:
+            result.append(f"{start}->{nums[-1]}")
+        
+        return result
+        
