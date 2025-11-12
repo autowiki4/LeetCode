4a3462127e583dca98dd800e116d9721040f581e
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-11-12 01:04:37 -0600

    LC-happy-number [Runtime: 0 ms; Memory: 17.9 MB]

--- hash-table/happy-number.py
+++ hash-table/happy-number.py
@@ -1,9 +1,15 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        seen = set()
-    
-        while n != 1 and n not in seen:
-            seen.add(n)
-            n = sum(int(digit) ** 2 for digit in str(n))
-        
-        return n == 1
+        cycle = set()
+        while True:
+            curr = 0
+            for d in str(n):
+                num = int(d)
+                curr += num**2
+            if curr in cycle:
+                return False
+            if curr == 1:
+                return True
+            cycle.add(curr)
+            n = curr
+            
