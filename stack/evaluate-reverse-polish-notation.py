090d8c64cd6607eb3e5311d72415978e03d8b4fb
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-11-13 18:22:33 -0600

    LC-evaluate-reverse-polish-notation [Runtime: 0 ms; Memory: 19.2 MB]

--- stack/evaluate-reverse-polish-notation.py
+++ stack/evaluate-reverse-polish-notation.py
@@ -1,28 +1,19 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        for token in tokens:
-            if token == '+':
-                current = stack[-2] + stack[-1]
-                stack.pop(-1)
-                stack.pop(-1)
-                stack.append(current)
-            elif token == '-':
-                current = stack[-2] - stack[-1]
-                stack.pop(-1)
-                stack.pop(-1)
-                stack.append(current)
-            elif token == '*':
-                current = stack[-2] * stack[-1]
-                stack.pop(-1)
-                stack.pop(-1)
-                stack.append(current)
-            elif token == '/':
-                current = int(stack[-2] / stack[-1])
-                stack.pop(-1)
-                stack.pop(-1)
-                stack.append(current)
+        oper = {'+', '-', '*', '/'}
+        for t in tokens:
+            if t in oper:
+                b = stack.pop()
+                a = stack.pop()
+                if t == '+':
+                    stack.append(a + b)
+                elif t == '-':
+                    stack.append(a-b)
+                elif t == '*':
+                    stack.append(a*b)
+                else:
+                    stack.append(int(a/b))
             else:
-                stack.append(int(token))
-        
+                stack.append(int(t))
         return stack[0]
