a454ab5a2cdc81f55f5c6b9a812fef3cd19a5133
Osewuike Igue <153636747+autowiki4@users.noreply.github.com>
2025-11-14 01:49:16 -0600

    LC-basic-calculator [Runtime: 39 ms; Memory: 18.6 MB]

---
+++ recursion/basic-calculator.py
@@ -0,0 +1,24 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        res = 0
+        sign = 1
+        num = 0
+        for c in s:
+            if c.isdigit():
+                num = num*10 + int(c)
+            elif c == '+' or c == '-':
+                res += sign*num
+                sign = 1 if c == '+' else -1
+                num = 0
+            elif c == '(':
+                stack.append(res)
+                stack.append(sign)
+                res = 0
+                sign = 1
+            elif c == ')':
+                res += sign * num
+                num = 0
+                res *= stack.pop()
+                res += stack.pop()
+        return res + sign*num
